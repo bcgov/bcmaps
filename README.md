a270e7a703e5b938e526bc72873ac8fb5a413006
Andy Teucher <andy.teucher@gov.bc.ca>
2015-06-02 11:00:05 -0700

    Add package infrastructure

---
+++ README.md
@@ -0,0 +1,28 @@
+*Add a project state badge. See https://raw.githubusercontent.com/BCDevExchange/docs/master/discussion/projectstates.md*
+
+---
+
+# <package name>
+
+Description of package
+
+### Features
+
+### Installation
+
+### Usage
+
+### Project Status
+
+### Getting Help or Reporting an Issue
+
+To report bugs/issues/feature requests, please file an [issue](https://github.com/bcgov/<pkg-name>/issues/).
+
+### How to Contribute
+
+If you would like to contribute to the package, please see our 
+[CONTRIBUTING](CONTRIBUTING.md) guidelines.
+
+### License
+
+Apache 2.0. See our [license](LICENSE) for more details.
---
+++ data-raw/process_regional_districts.R
@@ -0,0 +1,67 @@
+################################################################################
+## Convert Statistics Canada 2011 cencus divisions (which are equivalent to BC
+## Regional Districts) in t A BC only regional district map. The data are available here:
+## http://www12.statcan.gc.ca/census-recensement/2011/geo/bound-limit/files-fichiers/gcd_000b11a_e.zip
+## under the Statistics Canada Open License Agreement: http://www.statcan.gc.ca/eng/reference/licence-eng
+
+
+library("sp")
+library("rgdal")
+
+url <- "http://www12.statcan.gc.ca/census-recensement/2011/geo/bound-limit/files-fichiers/gcd_000b11a_e.zip"
+path <- "data-raw/census-divisions_statscan"
+original_can_cd <- "gcd_000b11a_e"
+original_bc_cd <- "bc_cd_albers"
+tolerance <- "100m"
+simplified_bc_cd <- paste0("bc_cd_albers_qgis_simplify_", tolerance)
+
+if (!file.exists(file.path(path, paste0(simplified_bc_cd, ".shp")))) {
+
+  if (!file.exists(file.path(path, paste0(original_bc_cd, ".shp")))) {
+
+    if (!file.exists(file.path(path, paste0(original_can_cd, ".shp")))) {
+      download.file(url, destfile = file.path(path, paste0(original_can_cd, ".zip")))
+      unzip(file.path(path, paste0(original_can_cd, ".zip")), exdir = path)
+    }
+
+    cd <- readOGR(dsn = path, layer = original_can_cd)
+
+    bc_cd <- cd[cd$PRUID == 59,]
+
+    ## Transform to BC Albers
+    bc <- spTransform(bc_cd, CRS("+init=epsg:3005"))
+
+    writeOGR(bc, path, original_bc_cd, driver = "ESRI Shapefile")
+  }
+  message("Go into QGIS, import ", original_bc_cd,
+      ", use the Simplify Geometries tool and simplify with a tolerance of ",
+      tolerance, " and save as '", simplified_bc_cd, ".shp'")
+}
+
+regional_districts_analysis <- readOGR(path, layer = original_bc_cd,
+                                       stringsAsFactors = FALSE)
+
+regional_districts_disp <- readOGR(path, layer = simplified_bc_cd,
+                                   stringsAsFactors = FALSE)
+
+regional_districts_analysis <- regional_districts_analysis[, "CDNAME"]
+names(regional_districts_analysis) <- "region_name"
+
+regional_districts_analysis$region_type <- "Regional District"
+regional_districts_analysis$region_type[regional_districts_analysis$region_name == "Northern Rockies"] <- "Regional Municipality"
+regional_districts_analysis$region_type[regional_districts_analysis$region_name == "Stikine"] <- "Unincorporated Area"
+
+regional_districts_disp <- regional_districts_disp[, "CDNAME"]
+names(regional_districts_disp) <- "region_name"
+
+regional_districts_disp$region_type <- "Regional District"
+regional_districts_disp$region_type[regional_districts_disp$region_name == "Northern Rockies"] <- "Regional Municipality"
+regional_districts_disp$region_type[regional_districts_disp$region_name == "Stikine"] <- "Unincorporated Area"
+
+
+## Export as geojson
+# library(geojsonio)
+# regional_districts_disp %>%
+#   spTransform(CRS("+init=epsg:4326")) %>%
+# geojson_write(file = "regional_districts.geojson",
+#               layer_options = "COORDINATE_PRECISION=4")
