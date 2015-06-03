d08007ba5bf2009736ec46cb68b852df95e7943d
Andy Teucher <andy.teucher@gov.bc.ca>
2015-06-03 11:15:20 -0700

    Edit airzones processing script

--- data-raw/process_airzones.R
+++ data-raw/process_airzones.R
@@ -1,13 +1,25 @@
+# Copyright 2015 Province of British Columbia
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+
 library("sp")
 library("rgdal")
 library("devtools")
 
-bc_albers <- "+proj=aea +lat_1=50 +lat_2=58.5 +lat_0=45 +lon_0=-126 +x_0=1000000 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
-
 airzones_databc <- "http://catalogue.data.gov.bc.ca/dataset/e8eeefc4-2826-47bc-8430-85703d328516/resource/4cea4b22-36a3-4ea7-9346-7e872e747076/download/bcairzonesalbersshp.zip"
-airzones_zip <- "data-raw/airzones.zip"
+airzones_zip <- "data-raw/airzones/airzones.zip"
 
-download.file(airzones_databc, airzones_zip)
+if (!file.exists(airzones_zip)){
+  download.file(airzones_databc, airzones_zip)
+}
 
 unzip(airzones_zip, exdir = "data-raw/airzones")
 
