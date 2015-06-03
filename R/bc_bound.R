bd7669e637e8ee4b3300a06ea4ed0485939a39a8
Andy Teucher <andy.teucher@gov.bc.ca>
2015-06-03 16:51:33 -0700

    Add documentation for bc_bound

---
+++ R/bc_bound.R
@@ -0,0 +1,18 @@
+#' BC Boundary
+#'
+#' Boundary of British Columbia at 1:7.5M scale
+#'
+#' @format A spatialPolygonsDataFrame defining the boundary of each British Columbia. Scale is 1:7.5M
+#'
+#'   The data slot contains three columns:
+#' \describe{
+#'   \item{\code{island}}{Name of island, if applicable}
+#'   \item{\code{island_e}}{English name of island, if applicable}
+#'   \item{\code{island_f}}{French name of island, if applicable}
+#' }
+#'
+#' @source 1:7.5M Atlas of Canada base maps, downloaded from
+#' \href{http://open.canada.ca/data/en/dataset/f77c2027-ed4a-5f6e-9395-067af3e9fc1e}{The Canadian Open Data Portal}
+#' under the \href{http://open.canada.ca/en/open-government-licence-canada}{Open Government License - Canada}.
+#'
+"bc_bound"
