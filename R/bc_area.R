92bd30ec44028b8730d71cc3d04f9113d0095404
Andy Teucher <andy.teucher@gov.bc.ca>
2016-01-12 15:01:52 -0800

    Add square miles and acres

--- R/bc_area.R
+++ R/bc_area.R
@@ -1,24 +1,31 @@
 #' The size of British Columbia
 #'
-#' Total area, Land area only, or Freshwater area only, in \eqn{km^2}, \eqn{m^2}, or \code{ha}.
+#' Total area, Land area only, or Freshwater area only, in the units of your choosing.
 #'
 #' The sizes are from \href{http://www.statcan.gc.ca/tables-tableaux/sum-som/l01/cst01/phys01-eng.htm}{Statistics Canada}
 #'
 #' @param what Which part of BC? One of \code{'total'} (default), \code{'land'}, or \code{'freshwater'}.
 #' @param units One of \code{'km2'} (square kilometres; default), \code{'m2'} (square metres),
-#'          or \code{'ha'} (hectares)
+#'          \code{'ha'} (hectares), \code{'acres'}, or \code{'sq_mi'} (square miles)
 #'
 #' @return The area of B.C. in the desired units (numeric vector).
 #' @export
 #'
 #' @examples
+#' ## With no arguments, gives the total area in km^2:
+#' bc_area()
+#'
+#' ## Get the area of the land only, in hectares:
 #' bc_area("land", "ha")
 bc_area <- function(what = "total", units = "km2") {
   what = match.arg(what, c("total", "land", "freshwater"))
-  units = match.arg(units, c("km2", "m2", "ha"))
+  units = match.arg(units, c("km2", "m2", "ha", "acres", "sq_mi"))
 
   val_km2 <- switch(what, total = 944735, land = 925186, freshwater = 19549)
-  ret <- switch(units, km2 = val_km2, m2 = km2_m2(val_km2), ha = km2_ha(val_km2))
+  ret <- switch(units, km2 = val_km2, m2 = km2_m2(val_km2), ha = km2_ha(val_km2),
+                acres = km2_acres(val_km2), sq_mi = km2_sq_mi(val_km2))
+
+  ret <- round(ret, digits = 0)
   structure(ret, names = paste(what, units, sep = "_"))
 }
 
@@ -28,4 +35,12 @@ km2_m2 <- function(x) {
 
 km2_ha <- function(x) {
   x * 100
+}
+
+km2_acres <- function(x) {
+  x * 247.105
+}
+
+km2_sq_mi <- function(x) {
+  x * 0.386102
 }
