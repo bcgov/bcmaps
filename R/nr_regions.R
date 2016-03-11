dcbdc88016258789d6fe808d982b450dcc013629
stephhazlitt <stephanie.hazlitt@gov.bc.ca>
2016-03-11 15:21:40 -0800

    Add nr_regions data

---
+++ R/nr_regions.R
@@ -0,0 +1,31 @@
+# Copyright 2015 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+
+#' British Columbia Ecoprovinces
+#'
+#' An ecoprovince is an area with consistent climatic or oceanography, relief
+#' and regional landforms. There are one oceanic, two marine / terrestrial and seven
+#' terrestrial ecoprovinces occurring within British Columbia. Ecoprovinces are
+#' meant to be mapped at 1:2,000,000 for use in provincial state of the
+#' environment reporting
+#'
+#' @format A spatialPolygonsDataFrame with the boundaries of British Columbia's
+#'   Natural Resource (NR) Regions, administrative boundaries established within NR Areas.
+#'
+#'
+#' @source Original data from
+#'   \href{http://catalogue.data.gov.bc.ca/dataset/natural-resource-nr-regions}{DataBC},
+#'    under the
+#'   \href{http://www.data.gov.bc.ca/local/dbc/docs/license/OGL-vbc2.0.pdf}{Open
+#'   Government License - British Columbia}.
+#'
+"nr_regions"
