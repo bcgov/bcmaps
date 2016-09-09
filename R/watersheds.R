6aefe780f0c36f587d891a4293f8d28a79d66c23
markjohnsonubc <mark.johnson@ubc.ca>
2016-09-09 15:07:01 -0700

    adding R/watersheds.R
    to enable a user to  do ?watersheds and get the metadata.

---
+++ R/watersheds.R
@@ -0,0 +1,29 @@
+# Copyright 2016 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+
+#' British Columbia Hydrometric Watershed Boundaries
+#'
+#' Watershed boundary delineated for Canada-BC hydrometric stations. 
+#' Currently, watersheds were delineated using 1:50,000 scale boundaries in 1996, 
+#' and many watersheds encompass entire drainages, instead of just the upstream watersheds. 
+#' 
+#'
+#' @format A spatialPolygonsDataFrame with the boundaries of 457 watersheds in British Columbia.
+#'   
+#'
+#' @source Original data from
+#'   \href{http://catalogue.data.gov.bc.ca/dataset/hydrology-hydrometric-watershed-boundaries}{DataBC},
+#'    under the
+#'   \href{http://www.data.gov.bc.ca/local/dbc/docs/license/OGL-vbc2.0.pdf}{Open
+#'   Government License - British Columbia}.
+#'
+"watersheds"
