e0caa1b8031984d2e870a82bf7e41701a15bee63
stephhazlitt <stephanie.hazlitt@gov.bc.ca>
2017-02-14 12:33:00 -0800

    documentation for watercourses_15M

---
+++ R/watercourses_15M.R
@@ -0,0 +1,29 @@
+# Copyright 2016 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+
+#' British Columbia watercourses at 15M scale.
+#'
+#' Watercourses at 15M scale for British Columbia.
+#' Currently, watersheds were delineated using 1:50,000 scale boundaries in 1996,
+#' and many watersheds encompass entire drainages, instead of just the upstream watersheds.
+#'
+#'
+#' @format A spatialPolygonsDataFrame with watercourses at the 15M scale in British Columbia.
+#'
+#'
+#' @source Original data from
+#'   \href{https://www.nrcan.gc.ca/earth-sciences/geography/topographic-information/free-data-geogratis/download-directory-documentation/17215}{},
+#'    under the
+#'   \href{http://open.canada.ca/en/open-government-licence-canada}{Open
+#'   Government License - Canada}.
+#'
+"watercourses_15M"
