59c965c1f1e5cd8698bc2a874a3ed67752edb370
Sam Albers <sam.albers@gov.bc.ca>
2017-11-17 09:23:31 -0800

    better test for each layer function

--- tests/testthat/test-data_functions.R
+++ tests/testthat/test-data_functions.R
@@ -1,21 +1,33 @@
 context("test layer download from bcmaps.rdata")
 
-test_that("test that the layer function and returns an sf object as default", {
+test_that("test that all sf layer function work without error and returns an sf object as default", {
   fn_names <- available_layers()$Item
   for (i in seq_along(fn_names)) {
-    expect_silent(layer <- get(fn_names[i])())
+    #cat("\n", fn_names[i]) #for debugging
+    expect_error(layer <- match.fun(fn_names[i])(), NA)
     expect_is(layer, "sf")
   }
 })
 
-# arizones failing. TODO: figure out why.
-#test_that("test that the layer function ", {
-#  fn_names <- available_layers()$Item
-#  for (i in seq_along(fn_names)) {
-#    cat("\n", fn_names[i])
-#    expect_silent(layer <- get(fn_names[i])(class="sp"))
-#    #expect_is(layer, "SpatialPolygonsDataFrame") TODO: figure out which class to test
-#  }
-#})
+
+test_that("test that all sp layer function work without error and return a Spatial* object ", {
+  fn_names_sp <- available_layers()$Item
+  for (i in seq_along(fn_names_sp)) {
+    #cat("\n", fn_names_sp[i]) #for debugging
+    expect_error(layer <- match.fun(fn_names_sp[i])(class="sp"), NA)
+
+    ## watercourses_* are Lines - different object
+    if(fn_names_sp[i] %in% c("watercourses_15M","watercourses_5M")){
+      expect_is(layer, "SpatialLinesDataFrame")
+    }
+
+    ## All the rest of the objects
+    if(!fn_names_sp[i] %in% c("watercourses_15M","watercourses_5M")){
+      expect_is(layer, "SpatialPolygonsDataFrame")
+    }
+  }
+})
+
+
 
 
