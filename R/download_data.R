0e72057e1c0fcd9c0a7c545bd534d4cf61639424
Andy Teucher <andy.teucher@gov.bc.ca>
2017-11-21 23:05:19 -0800

    Tidy and commenting

--- R/download_data.R
+++ R/download_data.R
@@ -16,6 +16,8 @@
 #' digital Biogeoclimatic Ecosystem Classification (BEC)
 #' Zone/Subzone/Variant/Phase map (version 10, August 31st, 2016).
 #'
+#' @param class
+#'
 #' @format An `sf` polygons object with B.C.'s Biogeoclimatic Ecosystem
 #' Classification (BEC) Zone/Subzone/Variant/Phase map
 #'
@@ -25,7 +27,9 @@
 #'   \href{https://www2.gov.bc.ca/gov/content?id=A519A56BC2BF44E4A008B33FCF527F61}{Open
 #'   Government Licence - British Columbia}.
 #'
-get_bec <- function(class = c("sf", "sp")) {
+#' @export
+#'
+bec <- function(class = c("sf", "sp")) {
   get_big_data("bec", class)
 }
 
@@ -39,7 +43,7 @@ get_big_data <- function(what, class= c("sf", "sp")) {
     check_write_to_data_dir(dir)
   }
 
-  ret_path <- download_file_from_release(fname, fpath)
+  ret_path <- download_file_from_release("latest", fname, fpath)
   if (ret_path != fpath)
     stop("Something went wrong. ", fname, " was written to an unexpected place.")
 
@@ -53,20 +57,20 @@ get_big_data <- function(what, class= c("sf", "sp")) {
 
 data_dir <- function() rappdirs::user_data_dir("bcmaps")
 
-check_write_to_data_dir <- function(dir = data_dir()) {
-  ans <- ask(paste("bcmaps would like to store data in the directory:",
+check_write_to_data_dir <- function(dir) {
+  ans <- ask(paste("bcmaps would like to store this layer in the directory:",
                    dir, "Is that okay?", sep = "\n"))
   if (!ans) stop("Exiting...", call. = FALSE)
 
   dir.create(dir, showWarnings = FALSE)
 }
 
-download_file_from_release <- function(file, path) {
-  release <- get_latest_release()
-  # assets <- list_release_assets(release$id)
-  assets <- release$assets
+download_file_from_release <- function(release = "latest", file, path) {
+  the_release <- get_gh_release(release)
+  assets <- the_release$assets
 
   the_asset <- which(vapply(assets, function(x) x$name, FUN.VALUE = character(1)) == file)
+
   if (!length(the_asset)) {
     stop("No assets matching filename ", file)
   } else if (length(the_asset) > 1) {
@@ -79,20 +83,19 @@ download_file_from_release <- function(file, path) {
   asset_id_file <- gsub("rds$", "gh_asset_id", path)
 
   if (file.exists(asset_id_file)) {
+    # Read the asset id of the previously written file
     old_asset_id <- as.character(readLines(asset_id_file, n = 1L, warn = FALSE))
-    if (old_asset_id != the_asset_id) {
-      ans <- ask(paste0("There is a newer version of ", basename(file),
-                             "available. Would you like to download it and store it at ",
-                             path, "?"))
-      if (!ans) {
-        download <- TRUE
-      } else {
-        download <- FALSE
-      }
-    } else {
+    if (old_asset_id == the_asset_id) {
+      # the current one on disk is the same asset as on GH so don't download again
       download <- FALSE
+    } else {
+      ans <- ask(paste0("There is a newer version of ", basename(file),
+                        "available. Would you like to download it and store it at ",
+                        path, "?"))
+      download <- !ans
     }
   } else {
+    # Hasn't been downloaded before, so must download it now
     download <- TRUE
   }
 
@@ -104,27 +107,22 @@ download_file_from_release <- function(file, path) {
     download_release_asset(the_asset_url, path)
   } else {
     message("Loading file from cache...\n")
-    invisible(path)
   }
+  invisible(path)
 }
 
-get_latest_release <- function() {
+get_gh_release <- function(release) {
   # List releases
   rels_resp <- httr::GET(base_url())
   httr::stop_for_status(rels_resp)
 
   rels <- httr::content(rels_resp)
-
-  # List most recent release
-  release_resp <- httr::GET(paste0(base_url(), "/", rels[[1]]$id))
-  httr::stop_for_status(release_resp)
-  httr::content(release_resp)
-}
-
-list_release_assets <- function(release_id) {
-  resp <- httr::GET(paste0(base_url(), "/", release_id, "/assets"))
-  httr::stop_for_status(resp)
-  httr::content(resp)
+  # Most recent release
+  if (release == "latest") {
+    rels[[1]]
+  } else {
+    rels[vapply(rels, function(x) x$id == as.character(release), FUN.VALUE = logical(1))][[1]]
+  }
 }
 
 download_release_asset <- function(asset_url, path) {
