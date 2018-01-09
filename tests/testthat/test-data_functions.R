582044ecad8ffb11c67ba9e6562e0f94f8a0b271
Andy Teucher <andy.teucher@gmail.com>
2018-01-09 12:14:05 -0800

    Only test local layers with shortcut functions in test-data_functions

--- tests/testthat/test-data_functions.R
+++ tests/testthat/test-data_functions.R
@@ -1,9 +1,11 @@
-context("test layer download from bcmaps.rdata")
+context("test layer load from bcmaps.rdata")
+
+avail <- available_layers()
+fn_names <- avail[avail$local & avail$shortcut_function, "layer_name"]
 
 test_that("test that all sf layer function work without error and returns an sf object as default", {
   skip_on_cran()
   skip_if_not_installed("bcmaps.rdata")
-  fn_names <- available_layers()$layer_name
   for (i in seq_along(fn_names)) {
     #cat("\n", fn_names[i]) #for debugging
     expect_error(layer <- match.fun(fn_names[i])(), NA)
@@ -15,10 +17,9 @@ test_that("test that all sf layer function work without error and returns an sf 
 test_that("test that all sp layer function work without error and return a Spatial* object ", {
   skip_on_cran()
   skip_if_not_installed("bcmaps.rdata")
-  fn_names_sp <- available_layers()$layer_name
-  for (i in seq_along(fn_names_sp)) {
+  for (i in seq_along(fn_names)) {
     #cat("\n", fn_names_sp[i]) #for debugging
-    expect_error(layer <- match.fun(fn_names_sp[i])(class="sp"), NA)
+    expect_error(layer <- match.fun(fn_names[i])(class = "sp"), NA)
     expect_is(layer, "Spatial")
   }
 })
