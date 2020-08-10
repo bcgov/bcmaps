0c5c7d71e27283a25654c697d75432f7a2219b7f
Sam Albers <sam.albers@gov.bc.ca>
2020-08-10 10:49:36 -0700

    test layer load without bcmapsdata

--- tests/testthat/test-data_functions.R
+++ tests/testthat/test-data_functions.R
@@ -1,13 +1,12 @@
-context("test layer load from bcmapsdata")
+context("test layer load")
 
-if (requireNamespace("bcmapsdata", quietly = TRUE)) {
-  avail <- available_layers()
-  fn_names <- avail[avail$local & avail$shortcut_function, "layer_name"]
-}
+
+avail <- available_layers()
+fn_names <- avail$layer_name
+
 
 test_that("test that all sf layer function work without error and returns an sf object as default", {
   skip_on_cran()
-  skip_if_not_installed("bcmapsdata")
   for (i in seq_along(fn_names)) {
     #cat("\n", fn_names[i]) #for debugging
     expect_error(layer <- match.fun(fn_names[i])(), NA)
@@ -18,10 +17,10 @@ test_that("test that all sf layer function work without error and returns an sf 
 
 test_that("test that all sp layer function work without error and return a Spatial* object ", {
   skip_on_cran()
-  skip_if_not_installed("bcmapsdata")
   for (i in seq_along(fn_names)) {
     #cat("\n", fn_names_sp[i]) #for debugging
     expect_error(layer <- match.fun(fn_names[i])(class = "sp"), NA)
     expect_is(layer, "Spatial")
   }
 })
+
