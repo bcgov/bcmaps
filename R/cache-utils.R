2ebfa2d30150dc4e7626a29cd9dd9a2e16359eb7
Sam Albers <sam.albers@gov.bc.ca>
2020-08-14 11:43:50 -0700

    add function to work with cache

---
+++ R/cache-utils.R
@@ -0,0 +1,64 @@
+# Copyright 2020 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+
+
+#' Show path of which files are stored locally
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' show_cached_files()
+#' }
+
+show_cached_files <- function() {
+  file.path(list.files(data_dir(), "*.rds", full.names = TRUE))
+}
+
+#' Delete cached files
+#'
+#' @param files_to_delete An optional argument to specify which files should be delete.
+#'        Defaults to deleting all files pausing for permission from user. If a subset
+#'        of files are specified, the files are immediately deleted.
+#'
+#' @export
+#' @examples
+#' \dontrun{
+#' ## See which files you have
+#' show_cached_files()
+#'
+#' ## Delete your whole cache
+#' delete_cache()
+#'
+#' ## Specify which files are deleted
+#' delete_cache(c('regional_districts.rds', 'bc_cities.rds'))
+#' }
+
+delete_cache <- function(files_to_delete = NULL) {
+  rds_files <- show_cached_files()
+
+  if(is.null(files_to_delete)) {
+    ans <- ask(paste0("These are all the files you currently have stored locally: \n",
+               paste0(rds_files, collapse = "\n"),
+               "\n \nAre you sure you want to delete all these files: \n"))
+    if(!ans) stop("Phewf! Glad you re-considered.", call. = FALSE)
+  } else {
+    rds_files <- file.path(data_dir(), add_rds_suffix(files_to_delete))
+  }
+
+  unlink(rds_files)
+}
+
+add_rds_suffix <- function(x) {
+  exts <- tools::file_ext(x)
+  ifelse(exts == "", paste0(x, ".rds"), x)
+}
