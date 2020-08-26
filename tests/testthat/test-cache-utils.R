b2013f42a4625a6ca9999b9dd1511986051a9aa6
Sam Albers <sam.albers@gov.bc.ca>
2020-08-26 15:20:23 -0700

    setup test with alternate cache

--- tests/testthat/test-cache-utils.R
+++ tests/testthat/test-cache-utils.R
@@ -7,5 +7,5 @@ test_that("the cache is deleted",{
   expect_is(airzones(ask = FALSE, force = TRUE), "sf")
   expect_true(delete_cache("airzones.rds"))
   delete_cache()
-  expect_equal(list.files(data_dir()), character(0))
+  expect_equal(list.files(data_dir(), pattern = "\\.rds"), character(0))
 })
