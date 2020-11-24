3cc624c485a0a7129cd6c30ab83bf6dc67b48f98
Sam Albers <sam.albers@gov.bc.ca>
2020-11-24 13:54:05 -0800

    add trailing / to test

--- tests/testthat/test-cded.R
+++ tests/testthat/test-cded.R
@@ -6,7 +6,7 @@ test_that("list_mapsheet_files works", {
   skip_on_cran()
   skip_if_offline()
 
-  res <- list_mapsheet_files("https://pub.data.gov.bc.ca/datasets/175624/82g")
+  res <- list_mapsheet_files("https://pub.data.gov.bc.ca/datasets/175624/82g/")
   expect_type(res, "character")
   expect_length(res, 60L)
   expect_equal(sum(grepl("^082g.+md5$", res)), 30)
