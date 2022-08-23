c72e92c1f6fc84b82fe12d6043a7e0a5b74a463e
Andy Teucher <andy.teucher@gov.bc.ca>
2022-08-23 14:06:45 -0700

    (GHA failures) - remove double slashes from url

--- R/cded.R
+++ R/cded.R
@@ -249,8 +249,10 @@ check_hashes <- function(tiles_have, tiles_need, url) {
   remote_hashes <- vapply(md5_files, function(f) {
     tmp <- tempfile()
     on.exit(unlink(tmp), add = TRUE)
-    res <- httr::GET(paste0(url, "/", basename(f)),
-               httr::write_disk(tmp))
+    res <- httr::GET(gsub("(?<!:)/+", "/",
+                          paste0(url, "/", basename(f)),
+                          perl = TRUE),
+                     httr::write_disk(tmp))
     httr::stop_for_status(res)
     readChar(tmp, nchars = 40)
   }, character(1))
