a195d95a3eac712d21e7d64252ad3fed9b5735e4
Andy Teucher <andy.teucher@gmail.com>
2023-04-06 16:32:40 -0700

    Guard against NULL caller names (hit in tests using match.call())

--- R/get_data.R
+++ R/get_data.R
@@ -37,8 +37,9 @@ get_layer <- function(layer, class = deprecated(), ask = TRUE, force = FALSE) {
     # by one of the shortcut functions. caller will be NULL if
     # it's called from the global environment.
     caller <- rlang::caller_call()
-    if (is.null(caller) ||
-        !rlang::call_name(caller) %in% c(shortcut_layer_names(), "combine_nr_rd")) {
+    caller_name <- rlang::call_name(caller)
+    if (is.null(caller) || is.null(caller_name) ||
+        !caller_name %in% c(shortcut_layer_names(), "combine_nr_rd")) {
       deprecate_sp('bcmaps::get_layer(class)')
     }
     class <- match.arg(class, choices = c('sf', 'sp'))
